480794da978a47d8a2e7e039305418380c39b794
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-27 23:58:51 +0530

    all

---
+++ docker/main.tf
@@ -0,0 +1,101 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# ✅ VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# ✅ Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# ✅ Subnet (Public)
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# ✅ Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# ✅ Route to Internet
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# ✅ Associate Route Table
+resource "aws_route_table_association" "assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# ✅ Security Group (ONLY SSH)
+resource "aws_security_group" "sg" {
+  name   = "ssh-only-sg"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ✅ EC2 (Docker + /var extend)
+resource "aws_instance" "docker_ec2" {
+  ami           = local.ami_id
+  instance_type = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 50
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+
+              dnf update -y
+
+              # Install Docker
+              sudo dnf -y install dnf-plugins-core
+              sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+              sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ec2-user
+
+              # Extend disk
+              growpart /dev/nvme0n1 4
+              pvresize /dev/nvme0n1p4
+
+              # Remove /home
+              lvremove -y /dev/RootVG/homeVol
+
+              # Extend /var
+              lvextend -r -L +30G /dev/RootVG/varVol
+
+              EOF
+
+  tags = {
+    Name = "Docker-EC2"
+  }
+}
---
+++ docker/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+    default = "docker-practice"
+  
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+  
+}
