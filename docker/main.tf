910952d87575c56e420f31fb93e0bf97cc8bf1a7
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-28 19:12:38 +0530

    all

--- docker/main.tf
+++ docker/main.tf
@@ -71,13 +71,13 @@ resource "aws_instance" "docker_ec2" {
 user_data = <<-EOF
 #!/bin/bash
 
-yum update -y
-
 # Install Docker (simple & reliable)
-yum install -y docker
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
 
 # Install LVM tools
 yum install -y lvm2 cloud-utils-growpart
